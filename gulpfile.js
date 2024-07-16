e93a9ada628bcf62d0d4e0ad27703c5254cff804
Max Lawrence <lavrentievmx@gmail.com>
2024-07-16 12:53:45 +0300

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -12,10 +12,18 @@ import browserSync from 'browser-sync';
 const config = {
     demo: 'demo',
     build: 'build',
-    watch: {
-        scss: ['src/**/*.scss'],
-        scripts: ['src/**/*.js'],
-        demo: ['demo/**/*.html', 'demo/**/*.css']
+    routes: {
+        scss: {
+            src: ['src/**/*.scss'],
+            watch: ['src/**/*.scss']
+        },
+        js: {
+            src: ['src/**/*.js'],
+            watch: ['src/**/*.js']
+        },
+        demo: {
+            watch: ['demo/**/*.html',  'demo/**/*.css']
+        }
     }
 }
 
@@ -39,7 +47,7 @@ const do_reload = (done) => {
 }
 
 const do_scss = () => {
-    return gulp.src(config.watch.scss, { base: 'src' })
+    return gulp.src(config.routes.scss.src, { base: 'src' })
     .pipe(cached('cache_scss'))
     .pipe(debug())
     .pipe(gsass(sass)())
@@ -47,24 +55,24 @@ const do_scss = () => {
     .pipe(gulp.dest(config.build));
 }
 
-const do_scripts = () => {
-    return gulp.src(config.watch.scripts, { base: 'src' })
-    .pipe(cached('cache_scripts'))
+const do_js = () => {
+    return gulp.src(config.routes.js.src, { base: 'src' })
+    .pipe(cached('cache_js'))
     .pipe(debug())
     .pipe(gulp.dest(config.build));
 }
 
 const do_watch = (done) => {
-    gulp.watch(config.watch.scss, gulp.series(do_scss, do_reload));
-    gulp.watch(config.watch.scripts, gulp.series(do_scripts, do_reload));
-    gulp.watch(config.watch.demo, gulp.series(do_reload));
+    gulp.watch(config.routes.scss.watch, gulp.series(do_scss, do_reload));
+    gulp.watch(config.routes.js.watch, gulp.series(do_js, do_reload));
+    gulp.watch(config.routes.demo.watch, gulp.series(do_reload));
     done();
 }
 
 const build = gulp.series(
     do_clean,
     do_scss,
-    do_scripts,
+    do_js,
     do_watch,
     do_browserSync
 )
